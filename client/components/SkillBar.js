27d1875f5419e383991d6a60ebda4e01a7f77ab3
Kyle Morehead <krmorehead@gmail.com>
2017-05-02 18:09:46 -0700

    Sort by will now be asc or desc depending on number of clicks"

--- client/components/SkillBar.js
+++ client/components/SkillBar.js
@@ -3,17 +3,25 @@ class SkillBar extends React.Component{
     super(props)
     this.state = {};
     this.state.skills = getSkills()
-    this.columns = [{value: 'Skill Name', slug: 'name'}, {value: 'Attr.', slug: 'attr'}, {value: 'Bonus', slug: 'bonus'}];
+    this.columns = [{value: 'Skill Name', slug: 'name', sort: 'as'}, {value: 'Attr.', slug: 'attr'}, {value: 'Bonus', slug: 'bonus'}];
+    this.sortProperty = 'name';
+    this.asc = true;
 
     this.sortBy = (slug) => {
       var sortedSkills;
       var stats = this.props.stats;
+      if (this.sortProperty === slug) {
+        this.asc = !this.asc;
+      }
+      var orderBy = this.asc ? 'asc' : 'desc';
+
+      this.sortProperty = slug;
       if (slug === 'bonus') {
-        sortedSkills = _.sortBy(this.state.skills, function (skill1) {
+        sortedSkills = _.orderBy(this.state.skills, function (skill1) {
           return -calculateBonus(stats[skill1.attr].value);
-        })
+        }, orderBy)
       } else {
-        sortedSkills = _.sortBy(this.state.skills, slug);
+        sortedSkills = _.orderBy(this.state.skills, slug, orderBy);
 
       }
       this.setState({skills: sortedSkills});
